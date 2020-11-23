69817e8ff6dcd2dec42bedf4fe3830779f83a2dd
DmitriyZhevnov <dmitriy.zhevnov04@mail.ru>
2020-11-23 17:16:21 +0300

    добавлены новые товары в магазин

--- src/main/java/classes/StorageOfProducts.java
+++ src/main/java/classes/StorageOfProducts.java
@@ -9,8 +9,11 @@ public class StorageOfProducts {
 
     public StorageOfProducts() {
         addProduct(new Product(1, "Milk", "2.3%", 1.50), 15);
-        addProduct(new Product(2, "Water", "sparkling ", 1.00), 10);
-        addProduct(new Product(3, "Apple", "red", 0.10), 5);
+        addProduct(new Product(2, "Water", "sparkling ", 1.00), 15);
+        addProduct(new Product(3, "Apple", "red", 0.10), 15);
+        addProduct(new Product(4, "Pizza", "4 seasons", 15.00), 15);
+        addProduct(new Product(5, "Pizza", "Margarita", 13.00), 15);
+        addProduct(new Product(6, "Sugar", "1 kg", 3.00), 15);
     }
 
     public static void addProduct(Product product, int countToAdd) {
